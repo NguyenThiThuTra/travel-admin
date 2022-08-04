ca49d134cde9224aadfbab9a559a6af50a38f92f
NguyenThiThuTra <tra.ntt180999@gmail.com>
2022-08-04 22:15:25 +0700

    fix redux rejected

--- src/features/Homestay/HomestaySlice.js
+++ src/features/Homestay/HomestaySlice.js
@@ -128,6 +128,7 @@ const homestaysSlices = createSlice({
       state.loading = false;
     },
     [fetchAllHomestays.rejected]: (state, action) => {
+      state.homestays = [];
       state.loading = false;
       state.error = action.payload;
     },
@@ -142,6 +143,7 @@ const homestaysSlices = createSlice({
       state.loading = false;
     },
     [fetchAllHomestaySearch.rejected]: (state, action) => {
+      state.homestays = [];
       state.loading = false;
       state.error = action.payload;
     },
